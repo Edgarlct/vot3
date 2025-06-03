4708630ae9d34c93cc8a4969dd6212a354d32db2
Edgarlct <edgar.lecomte@icloud.com>
2025-06-03 15:08:05 +0200

    Add proposal creation functionality to debate form; integrate contract interaction

--- front/src/pages/Create.vue
+++ front/src/pages/Create.vue
@@ -12,15 +12,23 @@ import { useWeb3Store } from '../stores/web3';
 import DebateForm from '../components/DebateForm.vue';
 import ModalConnect from '../components/ModalConnect.vue';
 import type { Debate } from '../types';
+import Vote from '../abi/Vote.json';
+import {writeContract} from "../../utils/wallet.ts";
 
 const store = useWeb3Store();
 const showModal = ref(false);
 
-function handleCreate(newDebate: Debate) {
+async function handleCreate(newDebate: Debate) {
   if (!store.isConnected) {
     showModal.value = true;
     return;
   }
+
+  await writeContract(
+    Vote.abi,
+    "createProposal",
+    [newDebate.question, newDebate.category, newDebate.expiatedAt],
+  );
 
   console.log('Débat créé par', store.userAddress, ':', newDebate);
 }
